1cd7e5252a8e90ac0f8750b34752ba89dbe161bc
wangk0b <71711006+wangk0b@users.noreply.github.com>
2025-07-04 16:24:57 +0300

    Delete doc-crawler-main directory

--- doc-crawler-main/doc_crawler/__init__.py
+++
@@ -1,4 +0,0 @@
-from .crawller import Crawller
-from .utils.shared_utils import get_logger, sanitize_filename
-
-__all__ = ["Crawller"]
--- doc-crawler-main/doc_utils/redis_cache.py
+++
@@ -1,5 +0,0 @@
-import redis 
-
-def get_redis_connection():
-    r = redis.Redis(host='localhost', port=6379, decode_responses=True)
-    return r
